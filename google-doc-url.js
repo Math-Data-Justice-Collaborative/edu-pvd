1fc034080458fd1adce32866465b347bf5f633a2
jfang2121 <jfang@bates.edu>
2024-05-28 10:24:57 -0500

    push

---
+++ google-doc-url.js
@@ -0,0 +1,6 @@
+// Use the Google Sheets template to create the Google Sheet that you need.
+// Then Publish the Google Sheet as csv
+
+// paste in your published Google Sheets URL from the browser address bar
+
+https://docs.google.com/spreadsheets/d/e/2PACX-1vTw5t4Ea9oSMp98mNQooQSxjJtaEPgvmxT1lqY9JyUuRY-STK28MZ0VHqKhpQPEpunCULcSxF3vcG5u/pub?output=csv
